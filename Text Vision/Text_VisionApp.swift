97d3b7ee0c2925d67e439a6a1e84288f5defc7e1
Jacob <89097881+jacobd082@users.noreply.github.com>
2022-03-26 13:45:28 -0500

    Initial Commit

---
+++ Text Vision/Text_VisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Text_VisionApp.swift
+//  Text Vision
+//
+//  Created by Jacob Drath on 3/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Text_VisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
